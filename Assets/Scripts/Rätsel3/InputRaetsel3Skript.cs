f11559acc8f8a360cfef0a91359320b628b053c6
liviafhgr <livia.schwarz@unifr.ch>
2025-05-20 16:03:55 +0200

    add InputRaetsel3Skript (neuaufsetzung nach absturz)

--- Assets/Scripts/Rätsel3/InputRaetsel3Skript.cs
+++ Assets/Scripts/Rätsel3/InputRaetsel3Skript.cs
@@ -1,18 +1,31 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class InputRaetsel3Skript : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public GameObject sendingBoxContent;   // Hier liegt das ChatInputHandler-Skript drauf
+    public GameObject prefab3121;          // Prefab für richtige Antwort
+    public GameObject prefab3122;          // Prefab für falsche Antwort
+    public Transform receiveBoxContent;    // Ziel-Container für Instanziierung
 
-    // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Return))
+        {
+            StartCoroutine(CheckInputNextFrame());
+        }
+    }
+
+    IEnumerator CheckInputNextFrame()
+    {
+        yield return null; // Einen Frame warten, damit playerInput gesetzt ist
+        var inputScript = sendingBoxContent.GetComponent<ChatInputHandler>();
+        if (inputScript != null && !string.IsNullOrEmpty(inputScript.playerInput))
+        {
+            if (inputScript.playerInput == "6")
+                Instantiate(prefab3121, receiveBoxContent);
+            else
+                Instantiate(prefab3122, receiveBoxContent);
+        }
     }
 }

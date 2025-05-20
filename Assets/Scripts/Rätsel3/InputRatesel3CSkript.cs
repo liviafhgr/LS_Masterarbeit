fd72a85b43862d2482ed1ae11c7234ca940c3674
liviafhgr <livia.schwarz@unifr.ch>
2025-05-20 17:33:25 +0200

    add hinweis3

--- Assets/Scripts/Rätsel3/InputRatesel3CSkript.cs
+++ Assets/Scripts/Rätsel3/InputRatesel3CSkript.cs
@@ -6,13 +6,10 @@ public class InputRatesel3CSkript : MonoBehaviour
     public GameObject sendingBoxContent;   // GameObject mit ChatInputHandler
     public GameObject prefab3201;          // Prefab für richtige Antwort ("2")
     public GameObject prefab3202;          // Prefab für falsche Antwort
+    public GameObject hinweis3;            // Prefab für Hinweis nach 3 Fehlern
     public Transform receiveBoxContent;    // Ziel-Container für Instanziierung
 
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+    private int falscheAntwortCounter = 0; // Zähler für prefab3202
 
     // Update is called once per frame
     void Update()
@@ -36,6 +33,11 @@ public class InputRatesel3CSkript : MonoBehaviour
             else
             {
                 Instantiate(prefab3202, receiveBoxContent);
+                falscheAntwortCounter++;
+                if (falscheAntwortCounter == 3 && hinweis3 != null)
+                {
+                    Instantiate(hinweis3, receiveBoxContent);
+                }
             }
         }
     }

6a1e7a68cfcc15ce384416e51108328ac764b5be
liviafhgr <livia.schwarz@unifr.ch>
2025-05-20 16:34:06 +0200

    add hinweis1

--- Assets/Scripts/Rätsel3/InputRaetsel3Skript.cs
+++ Assets/Scripts/Rätsel3/InputRaetsel3Skript.cs
@@ -3,10 +3,13 @@ using UnityEngine;
 
 public class InputRaetsel3Skript : MonoBehaviour
 {
-    public GameObject sendingBoxContent;   // Hier liegt das ChatInputHandler-Skript drauf
+    public GameObject sendingBoxContent;   // GameObject mit ChatInputHandler
     public GameObject prefab3121;          // Prefab für richtige Antwort
     public GameObject prefab3122;          // Prefab für falsche Antwort
+    public GameObject hinweis1;            // Prefab für Hinweis nach 3 Fehlern
     public Transform receiveBoxContent;    // Ziel-Container für Instanziierung
+
+    private int falscheAntwortCounter = 0; // Zähler für prefab3122
 
     void Update()
     {
@@ -23,9 +26,23 @@ public class InputRaetsel3Skript : MonoBehaviour
         if (inputScript != null && !string.IsNullOrEmpty(inputScript.playerInput))
         {
             if (inputScript.playerInput == "6")
+            {
                 Instantiate(prefab3121, receiveBoxContent);
+            }
             else
+            {
                 Instantiate(prefab3122, receiveBoxContent);
+                falscheAntwortCounter++;
+                if (falscheAntwortCounter == 3 && hinweis1 != null)
+                {
+                    Instantiate(hinweis1, receiveBoxContent);
+                }
+            }
         }
+    }
+
+    public void CheckInput()
+    {
+        StartCoroutine(CheckInputNextFrame());
     }
 }

b7fb2c7bac60e3c8c6ad2f2a69343db844bf628d
liviafhgr <livia.schwarz@unifr.ch>
2025-05-21 15:29:15 +0200

    add InputRatesel4CSkript

--- Assets/Scripts/Rätsel4/InputRatesel4CSkript.cs
+++ Assets/Scripts/Rätsel4/InputRatesel4CSkript.cs
@@ -1,7 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class InputRatesel4CSkript : MonoBehaviour
 {
+    public GameObject sendingBoxContent;   // GameObject mit ChatInputHandler
+    public GameObject prefab41991;         // Prefab für richtige Antwort
+    public GameObject prefab4192;          // Prefab für falsche Antwort
+    public GameObject prefab420;           // Prefab für nächste Nachricht
+    public Transform receiveBoxContent;    // Ziel-Container für Instanziierung
+
+    // Liste aller erlaubten Schreibweisen für "simonetta"
+    private readonly HashSet<string> simonettaVarianten = new HashSet<string>()
+    {
+        "simonetta", "simoneta", "simonneta", "simmonetta", "simonett@", "s1monetta", "simonetta!", "sim0netta"
+    };
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -11,6 +25,35 @@ public class InputRatesel4CSkript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Return))
+        {
+            StartCoroutine(CheckInputNextFrame());
+        }
+    }
+
+    public void CheckInput()
+    {
+        StartCoroutine(CheckInputNextFrame());
+    }
+
+    IEnumerator CheckInputNextFrame()
+    {
+        yield return null;
+        var inputScript = sendingBoxContent.GetComponent<ChatInputHandler>();
+        if (inputScript != null && !string.IsNullOrWhiteSpace(inputScript.playerInput))
+        {
+            string eingabe = inputScript.playerInput.Trim().ToLower();
+            if (simonettaVarianten.Contains(eingabe))
+            {
+                Instantiate(prefab41991, receiveBoxContent);
+                yield return new WaitForSeconds(1f);
+                if (prefab420 != null)
+                    Instantiate(prefab420, receiveBoxContent);
+            }
+            else
+            {
+                Instantiate(prefab4192, receiveBoxContent);
+            }
+        }
     }
 }

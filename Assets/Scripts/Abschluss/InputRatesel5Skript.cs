ae8378199bcf90493a98282f23fa2e92e10fc165
liviafhgr <livia.schwarz@unifr.ch>
2025-05-24 08:46:53 +0200

    done InputRatesel5Skript

--- Assets/Scripts/Abschluss/InputRatesel5Skript.cs
+++ Assets/Scripts/Abschluss/InputRatesel5Skript.cs
@@ -6,7 +6,12 @@ public class InputRatesel5Skript : MonoBehaviour
     public GameObject sendingBoxContent;   // GameObject mit ChatInputHandler
     public GameObject prefab561;           // Prefab für richtige Antwort
     public GameObject prefab562;           // Prefab für falsche Antwort
+    public GameObject prefab563;           // Prefab für Hinweis nach 3/4 Fehlern
+    public GameObject prefab564;           // Prefab für Hinweis nach 5/6 Fehlern
+    public GameObject prefab565;           // Prefab für Hinweis ab 7 Fehlern
     public Transform receiveBoxContent;    // Ziel-Container für Instanziierung
+
+    private int wrongCount = 0;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -42,6 +47,23 @@ public class InputRatesel5Skript : MonoBehaviour
             else
             {
                 Instantiate(prefab562, receiveBoxContent);
+                wrongCount++;
+
+                if (wrongCount == 3 || wrongCount == 4)
+                {
+                    if (prefab563 != null)
+                        Instantiate(prefab563, receiveBoxContent);
+                }
+                else if (wrongCount == 5 || wrongCount == 6)
+                {
+                    if (prefab564 != null)
+                        Instantiate(prefab564, receiveBoxContent);
+                }
+                else if (wrongCount >= 7)
+                {
+                    if (prefab565 != null)
+                        Instantiate(prefab565, receiveBoxContent);
+                }
             }
         }
     }
